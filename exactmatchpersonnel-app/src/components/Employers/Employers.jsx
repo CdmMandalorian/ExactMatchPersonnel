fcf5d450565f782e9b0fd0df421bb1683517750a
CdmMandalorian <68617257+CdmMandalorian@users.noreply.github.com>
2021-11-17 17:40:29 -0800

    started dev work on employers page

--- exactmatchpersonnel-app/src/components/Employers/Employers.jsx
+++ exactmatchpersonnel-app/src/components/Employers/Employers.jsx
@@ -5,25 +5,21 @@ import logo from '../../Assets/Images/EMPlogo.png'
 
 function Employers() {
     return (
-        <Container fluid className="about-container">
+        <Container fluid className="employers-container">
       <Container>
         <h1 style={{ fontSize: '2.1em', color: 'rgba(245, 181, 27)' }}>
           Employers
         </h1>
-        <Row className='about-description'>
+        <Row className='employers-description'>
           <Col style={{ color: 'white' }}>
             <h3>
-              Exact Match Personnel is a Staffing Agency.
+              Looking for your next Candidates?
             </h3>
           </Col>
         </Row>
         <Row>
-          <Col
-            md={5}
-            style={{ paddingTop: "20px", paddingBottom: "50px" }}
-            className="about-img"
-          >
-            <img src={logo} alt="aboutLogo" className="img-fluid" />
+          <Col >
+
           </Col>
         </Row>
       </Container>
