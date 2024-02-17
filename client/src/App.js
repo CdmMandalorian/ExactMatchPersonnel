37c299be69c8c62b56b9752f7062374ac2bef6c0
CdmMandalorian <68617257+CdmMandalorian@users.noreply.github.com>
2024-02-17 09:11:25 -0800

    swap navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import Preloader from '../src/components/Pre'
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/Navbar/Navbar4';
 import { Route , Routes} from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './components/Home/Home';
---
+++ client/src/components/Navbar/Navbar4.js
@@ -0,0 +1,28 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from '../../Assets/navbarlogo/EMPlogo2.png'
+
+function CollapsibleExample() {
+  return (
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/"><img src={logo} className="img-fluid logo" alt="brand"/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">HOME</Nav.Link>
+            <Nav.Link href="/about">ABOUT</Nav.Link>
+            <Nav.Link href="/mission">MISSION</Nav.Link>
+            <Nav.Link href="/services">SERVICES</Nav.Link>
+            <Nav.Link href="/contact">CONTACT</Nav.Link>
+            <Nav.Link href="/">APPLY</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default CollapsibleExample;
